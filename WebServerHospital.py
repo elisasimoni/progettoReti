7447d007b8c3d2ea29e4362bc7a885a48f5989e5
Michele Nardini <michele.nardini3g@gmail.com>
2021-05-19 13:06:44 +0200

    Update WebServerHospital.py

--- WebServerHospital.py
+++ WebServerHospital.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 This is a temporary script file.
 """
 import threading
@@ -39,45 +38,174 @@ link_center = """
 <H1 align="center">
     Servizi Ospedalieri Nardini e Simoni
 </H1>
-    <div align="center">
-       <p>
-            
-            <a href="http://127.0.0.1:{port}">Home</a>
-  		       <a href="http://127.0.0.1:{port}/rianimazione.html">Rianimazione</a>
-            <a href="http://127.0.0.1:{port}/ortopedia.html">Ortopedia</a>
-            <a href="http://127.0.0.1:{port}/ginecologia.html">Ginecologia</a>
-            <a href="http://127.0.0.1:{port}/cardiologia.html">Cardiologia</a>
-            <a href="http://127.0.0.1:{port}/otorino.html">Otorinolarigoiatria</a>
-            <a href="http://127.0.0.1:{port}/neurologia.html">Neurologia</a>
-            <a href="http://127.0.0.1:{port}/oculista.html">Oculista</a>
-            <a href="http://127.0.0.1:{port}/dermatologia.html">Dermatologia</a>
-            <a href="http://127.0.0.1:{port}/oncologia.html">Oncologia</a>
-            <a href="http://127.0.0.1:{port}/medicina_sport.html">Medicina dello sport</a>
-            <a href="http://127.0.0.1:{port}/nefrologia.html">Nefrologia</a>
-  		    
-            <a href="http://127.0.0.1:{port}/lista_servizi.pdf" download="lista_servizi.pdf">Download lista servizi</a>
-  		</p>
+    <div align="left" class="list">
+        <ul>
+            <br>
+            <li>
+                <a href="http://127.0.0.1:{port}/rianimazione.html">
+                    <h3>Rianimazione</h3>
+                    <p>La rianimazione e' la branca della medicina che si occupa del paziente in condizioni critiche e della cura e del ripristino delle funzioni vitali compromesse dall'insorgenza di una malattia acuta o di un evento traumatico.</p>
+                </a>
+            </li>
+            <br>
+            <li>
+                <a href="http://127.0.0.1:{port}/cardiologia.html">
+                    <h3>Cardiologia</h3>
+                    <p>La cardiologia e' quella branca della medicina interna che si occupa dello studio, della diagnosi e della cura (farmacologica e/o invasiva) delle malattie cardiovascolari acquisite o congenite.</p>
+                </a>
+            </li>
+            <br>
+            <li>
+                <a href="http://127.0.0.1:{port}/dermatologia.html">
+                    <h3>Dermatologia</h3>
+                    <p>La dermatologia (dal greco derma, pelle) e' la branca della medicina che si occupa delle patologie a carico della pelle e degli annessi cutanei (peli, capelli, unghie, ghiandole sudoripare).</p>
+                </a>
+            </li>
+            <br>
+            <li>
+                <a href="http://127.0.0.1:{port}/ginecologia.html">
+                    <h3>Ginecologia</h3>
+                    <p>La ginecologia e' una branca della medicina che si occupa talvolta della fisiologia, ma soprattutto della patologia inerenti all'apparato genitale femminile.</p>
+                </a>
+            </li>
+            <br>
+           <li>
+                <a href="http://127.0.0.1:{port}/medicina_sport.html">
+                    <h3>Medicina sportiva</h3>
+                    <p>La medicina dello sport, conosciuta anche come medicina dello sport e dell'esercizio fisico, e' una branca della medicina che si occupa dello sport, dell'esercizio fisico e delle patologie ad essi correlate.</p>
+                </a>
+            </li>
+            <br>
+            <li>
+                <a href="http://127.0.0.1:{port}/nefrologia.html">
+                    <h3>Nefrologia</h3>
+                    <p>La nefrologia e' quella branca della medicina interna che si occupa delle malattie renali.</p>
+                </a>
+            </li>
+            <br>
+            <li>
+                <a href="http://127.0.0.1:{port}/neurologia.html">
+                    <h3>Neurologia</h3>
+                    <p>La neurologia e' la branca specialistica della medicina che studia le patologie del sistema nervoso centrale , del sistema periferico somatico e del sistema nervoso periferico autonomo .</p>
+                </a>
+            </li>
+            <br>
+            <li>
+                <a href="http://127.0.0.1:{port}/oculista.html">
+                    <h3>Oculista</h3>
+                    <p>Un oculista e' un medico che ha anche conseguito il titolo di specialista in oftalmologia.</p>
+                </a>
+            </li>
+            <br>
+            <li>
+                <a href="http://127.0.0.1:{port}/oncologia.html">
+                    <h3>Oncologia</h3>
+                    <p>L'oncologia e' il ramo della medicina che studia i tumori dal punto di vista morfologico e clinico.</p>
+                </a>
+            </li>
+            <br>
+           <li>
+                <a href="http://127.0.0.1:{port}/ortopedia.html">
+                    <h3>Ortopedia</h3>
+                    <p>L'ortopedia e' una branca iperspecialistica della chirurgia che si occupa dello studio e del trattamento delle patologie dell'apparato locomotore.</p>
+                </a>
+            </li>
+            <li>
+                <a href="http://127.0.0.1:{port}/otorino.html">
+                    <h3>Otorino</h3>
+                    <p>L'otorinolaringoiatria e' la branca della medicina che si occupa di prevenzione, diagnosi e terapia sia medica sia chirurgica delle patologie del distretto testa-collo, ossia dell'orecchio (udito ed equilibrio), del naso (respirazione e apnee del sonno) e della gola</p>
+                </a>
+            </li>
+            </li> <a href="http://127.0.0.1:{port}/Servizi_offerti.pdf" download="Servizi_offerti.pdf">Download lista servizi</a></li>
+        </ul>
+        <br>
    </div>
 """.format(port=port)
 
-center_page = """ 
+page_style = """
+<style>
+    * {margin: 0; padding: 0;}
+ 
+    div {
+        margin: 20px;
+    }
+ 
+    ul {
+        list-style-type: none;
+        width: 500px;
+    }
+ 
+    h3 {
+        font: bold 20px/1.5 Helvetica, Verdana, sans-serif;
+    }
+ 
+    li p {
+        font: 200 12px/1.5 Georgia, Times New Roman, serif;
+    }
+ 
+    li {
+        float: left;
+        margin: 0 15px 0 0;
+        padding: 10px;
+        overflow: auto;
+    }
+ 
+    li:hover {
+        background: #eee;
+        cursor: pointer;
+    }
+    
+    .list a {
+        color: black;
+        text-decoration: none;
+    }
+</style>
+"""
 
+login_style = """
+<style>
+    form {
+        border: 3px solid #f1f1f1;
+    }
+    input[type=text], input[type=password] {
+        width: 50%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+    
+    button {
+        background-color: #04AA6D;
+        color: white;
+        padding: 12px 20px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 50%;
+    }
+</style>
+"""
+
+center_page = """ 
 <body>  
-    <center> <h1> Student Login Form </h1> </center> 
+    <center> <h1> Login Form </h1> </center> 
     <form>
         <div align="center">
         <div class="container"> 
             <label>Username : </label> 
             <input type="text" placeholder="Enter Username" name="username" required>
+            <br>
             <label>Password : </label> 
             <input type="password" placeholder="Enter Password" name="password" required>
             <button type="button"><a href="http://127.0.0.1:{port}/home.html">Login</a></button>
+            <br>
             <input type="checkbox" checked="checked"> Remember me
             Forgot password? </a> 
         </div> 
         </div>
     </form>   
-
 """.format(port=port)
 
 footer_html= """
@@ -100,13 +228,13 @@ print("Server running on port %s" % port)
 
 def autenthication_page():
     f = open("autenthication.html",'w', encoding="utf-8")  
-    message = header_html+center_page+footer_html
+    message = header_html+login_style+center_page+footer_html
     f.write(message)
     f.close()
 
 def home_page():
     f = open("home.html",'w', encoding="utf-8")  
-    message = header_html+link_center+footer_html
+    message = header_html+page_style+link_center+footer_html
     f.write(message)
     f.close()
 
@@ -183,5 +311,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
